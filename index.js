4a0a42609eb07eaf5e31b5692e6c0720a5f88743
JosephShin <joespam9999@gmail.com>
2022-04-08 16:54:41 -0400

    commit tree 44f58197e4d589c97bdecaf10e84a99ebb4cbb6c

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+
+
 function addingEventListener() {
+
+    const input = document.getElementById('input')
+
+    input.addEventListener('click',() => {
+        alert('I was clicked!')
+    })
+
 }
